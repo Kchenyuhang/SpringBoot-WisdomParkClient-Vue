bbf0282e7edb682ddc9f680db78141e61993b972
吴家浩 <1273163614@qq.com>
2020-06-10 11:06:09 +0800

    修改接口

--- src/views/fleamarket/FleaMy.vue
+++ src/views/fleamarket/FleaMy.vue
@@ -1,5 +1,45 @@
 <template>
-  <p>我的界面</p>
+  <div class="bg">
+    <div class="header">
+      <div class="icon">
+        <img src="https://student-m.oss-cn-hangzhou.aliyuncs.com/img/settings.png" />
+      </div>
+      <div class="inform">
+        <div class="left">
+          <div class="card">
+            <img
+              class="img"
+              src="http://ww1.sinaimg.cn/large/0064QvQTly1gfmxh6zrflj305k05k3yc.jpg"
+            />
+            <div class="mes">
+              <p class="wid">用户名称</p>
+              <p>昵称：xxxx</p>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="btn">
+            <p>个人主页></p>
+          </div>
+        </div>
+      </div>
+      <div class="count">
+        <div class="up">
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+          <p>0</p>
+        </div>
+        <div class="down">
+          <p>收藏</p>
+          <p>历史浏览</p>
+          <p>关注</p>
+          <p>粉丝</p>
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </div>
 </template>
 <script>
 export default {
@@ -16,4 +56,6 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "../../assets/scss/fleamarket/FleaMy.scss";
+</style>
