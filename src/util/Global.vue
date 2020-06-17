5391cfcd335265cb1257ff1f4034fb898caae59d
吴家浩 <1273163614@qq.com>
2020-06-17 15:10:15 +0800

    push

--- src/util/Global.vue
+++ src/util/Global.vue
@@ -1,14 +1,5 @@
 <script>
-// 曹
- const baseUrl = "http://118.31.21.206:8080";
-// 涛
-<<<<<<< HEAD
-//const baseUrl = "http://120.26.177.51:8080";
-=======
-const baseUrl = "http://120.26.177.51:8080";
->>>>>>> 2ff07e841fdb334c381ad221fae6348fb94f896e
-// 昌
-// const baseUrl = "http://10.40.215.50:8080";
+const baseUrl = "http://118.31.21.206:8080";
 const token = "";
 const user = {};
 export default {
