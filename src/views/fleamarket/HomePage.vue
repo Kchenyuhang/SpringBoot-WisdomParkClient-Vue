e5307ef3b4edf4f3b828ef4a92dc18d6d0c873b9
吴家浩 <1273163614@qq.com>
2020-06-24 16:29:32 +0800

    样式大修改

--- src/views/fleamarket/HomePage.vue
+++ src/views/fleamarket/HomePage.vue
@@ -39,6 +39,7 @@
         </div>
       </div>
     </div>
+    <p class="hotShop">热卖商品</p>
     <div class="inform">
       <div
         class="right"
@@ -49,12 +50,13 @@
         <img :src="item.goodsImgUrl.split('--**--')[0]" />
         <div class="left">
           <!-- 商品描述 -->
-          <h3>{{ item.goodsName.slice(0, 10) }}</h3>
+          <h3>{{ item.goodsName.slice(0, 5) }}</h3>
           <!-- 价格 -->
           <span>¥{{ item.goodsPrice }}</span>
         </div>
       </div>
     </div>
+    <p class="hotShop">最新发布</p>
     <div
       class="release"
       @scroll="doload()"
@@ -193,7 +195,7 @@ export default {
       this.url = this.GLOBAL.baseUrl + "/flea/goods/all";
       this.data = {
         currentPage: 0,
-        pageSize: 6
+        pageSize: 4
       };
       this.list = (await API.init(this.url, this.data, "post")).data;
       // this.count = this.list.length - 4;
--- src/views/fleamarket/Sell.vue
+++ src/views/fleamarket/Sell.vue
@@ -7,14 +7,9 @@
       <p>发布</p>
     </div>
     <textarea
-      style="border: 0;border-radius:5px;background-color:#ffffff;width: 375px;height: 50px;padding: 10px;resize: none;"
+      class="titles"
       placeholder="标题，品牌，型号"
       v-model="data.goodsName"
-    ></textarea>
-    <textarea
-      style="border:0;border-radius:5px;background-color:#ffffff;width: 375px;height: 50px;padding: 10px;resize: none;"
-      placeholder="描述下你的商品"
-      v-model="data.goodsDescription"
     ></textarea>
     <!-- 图片上传区域 -->
     <div class="upload">
@@ -47,39 +42,64 @@
         id="file"
       />
     </div>
+    <textarea
+      class="des"
+      placeholder="描述下你的商品"
+      v-model="data.goodsDescription"
+    ></textarea>
     <div class="list">
       <div class="price">
-        <input
-          type="number"
-          placeholder="请输入价格"
-          v-model="data.goodsPrice"
-        />
-        <input
-          type="text"
-          placeholder="请输入类型"
-          v-model="data.goodsMark"
-        />
-
-        <select
-          class="option"
-          name="bbxb"
-          id="selecte1"
-          v-model="id"
-          @click="get(id)"
-        >
-          <option value="">---请选择--</option>
-          <option
+        <div>
+          <span class="tips">价格:</span>
+          <textarea
+            type="number"
+            class="title"
+            placeholder="请输入价格"
+            v-model="data.goodsPrice"
+          />
+          </div>
+        <div>
+          <span class="tips">类型:</span>
+          <textarea
+            type="number"
+            class="title"
+            placeholder="请输入类型"
+            v-model="data.goodsPrice"
+          />
+        </div>
+        <div>
+          <span class="tips">选项:</span>
+          <textarea
+            type="number"
+            class="title"
+            placeholder="请输入类型"
+            v-model="data.goodsPrice"
+          />
+          <select
             class="option"
-            :value="item.pkFleaTypeId"
-            selected="selected"
-            v-for="(item, index) in type"
-            :key="index"
-          >{{ item.typeName }}</option>
-        </select>
+            name="bbxb"
+            id="selecte1"
+            v-model="id"
+            @click="get(id)"
+          >
+            <option value="">---请选择--</option>
+            <option
+              class="option"
+              :value="item.pkFleaTypeId"
+              selected="selected"
+              v-for="(item, index) in type"
+              :key="index"
+              >{{ item.typeName }}</option
+            >
+          </select>
+        </div>
+
         <!-- <p>{{this.imgstr}}</p> -->
         <!-- <button @click="getSell">确认发布</button> -->
-        <button @click="getSell">发布为商品</button>
-        <button @click="getSellReward">发布为悬赏</button>
+        <div class="two">
+          <button @click="getSell">发布为商品</button>
+          <button @click="getSellReward">发布为悬赏</button>
+        </div>
       </div>
     </div>
   </div>
@@ -199,29 +219,38 @@ export default {
 h4 {
   margin-top: 10px;
   font-weight: 20px;
-  margin-left: 10px;
+  margin-left: 20px;
   font-size: 13px;
 }
 .up-pic {
   width: 90px;
   height: 90px;
-  margin-left: 140px;
-  border: none;
+  margin-left: 40%;
+  // border: none;
 }
 .upload {
-  height: 100px;
+  height: 90px;
+  width: 90%;
+  padding-left: 20px;
+  margin-top: 30px;
   // background-color: blue;
 }
 .list {
-  height: 200px;
+  height: 100px;
   margin-top: 10px;
   // background-color: brown;
+}
+.tips {
+  // margin-top: 20px;
+  position: relative;
+  bottom: 15px;
+  left: 10px;
 }
 .price input {
   height: 30px;
   width: 100%;
   margin-top: 5px;
-  border-radius: 10px;
+  border-radius: 4px;
   border: none;
 }
 select {
@@ -231,9 +260,10 @@ select {
 }
 button {
   height: 40px;
-  width: 100%;
-  margin-top: 80px;
-  background-color: rgb(0, 133, 215);
+  width: 100px;
+  margin: 20px;
+  // background-color: rgb(0, 133, 215);
+  background-image: linear-gradient(to right, #1e88e5, #1170bc);
   border: none;
   color: white;
   border-radius: 5px;
@@ -241,11 +271,53 @@ button {
   cursor: pointer;
 }
 button:hover {
-  background-color: rgb(17, 112, 188);
+  // background-color: rgb(17, 112, 188);
 }
 .option {
   width: auto;
   position: absolute;
   left: 50%;
+  margin-top: 5px;
+}
+.titles {
+  border: 0;
+  border-radius: 5px;
+  background-color: #ffffff;
+  width: 90%;
+  height: 40px;
+  padding: 10px;
+  resize: none;
+  border: 1px solid rgb(191, 191, 191);
+  margin-left: 20px;
+}
+.title {
+  border: 0;
+  border-radius: 5px;
+  background-color: #ffffff;
+  width: 80%;
+  height: 40px;
+  padding: 10px;
+  resize: none;
+  border: 1px solid rgb(191, 191, 191);
+  margin-left: 20px;
+}
+.des {
+  border: 0;
+  border-radius: 5px;
+  background-color: #ffffff;
+  width: 90%;
+  height: 150px;
+  padding: 10px;
+  resize: none;
+  border: 1px solid rgb(191, 191, 191);
+  margin-left: 20px;
+  margin-top: 50px;
+}
+.two {
+  display: flex;
+  justify-content: space-between;
+}
+textarea::placeholder {
+  margin-left: 100px;
 }
 </style>
