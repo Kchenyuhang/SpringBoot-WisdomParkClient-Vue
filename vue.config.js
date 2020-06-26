46ff4d452b66c7dceb8fbb5f9823ba4c97fad5a7
qixiao <zhengliang008@qq.com>
2020-06-26 22:38:46 +0800

    解决部分界面适配问题

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   devServer: {
-    host: 'localhost',
+    host: "localhost",
     port: 8088,
     https: false,
     open: true,
     hotOnly: true,
 
     proxy: {
-      '/a': {
-        target: 'http://120.26.185.155:8079',
+      "/a": {
+        target: "http://120.26.185.155:8079",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-          '^/a': '',
-        },
+          "^/a": ""
+        }
         // pathRewrite:{'.+?/api':'/api'},
-      },
-    },
+      }
+    }
   },
 
-  outputDir: './dist',
-  assetsDir: './assets/',
-  publicPath: './',
-  indexPath: './index.html',
-  transpileDependencies: ['vuetify'],
-}
+  outputDir: "./dist",
+  assetsDir: "./assets/",
+  publicPath: "./",
+  indexPath: "./index.html",
+  transpileDependencies: ["vuetify"]
+};
