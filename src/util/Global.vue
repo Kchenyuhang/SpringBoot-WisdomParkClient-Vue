7bbb2e25262fd0c5c4dc60a9055772943ea23508
qixiao <zhengliang008@qq.com>
2020-06-27 15:58:20 +0800

    解决数字输入框出现负值以及其他字符

--- src/util/Global.vue
+++ src/util/Global.vue
@@ -1,6 +1,7 @@
 <script>
 // const baseUrl = "http://118.31.21.206:8080";
-const baseUrl = "http://118.31.21.206:80";
+// const baseUrl = "http://118.31.21.206:80";
+const baseUrl = "http://localhost:80";
 const token = "";
 const user = {};
 export default {
